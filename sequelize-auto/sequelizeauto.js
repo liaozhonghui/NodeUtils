b6b2a2e09b7c1143b7aeb83b130d70625098ec39
zhonghui.liao <zhonghui.liao@zhongheschool.com>
2021-05-12 15:54:07 +0800

    modify format

--- sequelize-auto/sequelizeauto.js
+++ sequelize-auto/sequelizeauto.js
@@ -3,29 +3,29 @@ const SequelizeAuto = require('sequelize-auto');
 const ENV = process.env.NODE_ENV || 'development';
 const program = require('commander');
 const options = program
-    .option('-e, --env', 'env variable.')
-    .option('-s, --sync', 'sync')
-    .parse(process.argv)
-    .opts();
+  .option('-e, --env', 'env variable.')
+  .option('-s, --sync', 'sync')
+  .parse(process.argv)
+  .opts();
 
 const config = require('./database/config.json');
 if (!has(config, ENV)) {
-    console.error('env set is wrong.');
-    process.exit(0);
+  console.error('env set is wrong.');
+  process.exit(0);
 }
 
 const sequelizeConfig = get(config, ENV);
 const auto = new SequelizeAuto(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, merge(
-    {
-        caseFile: 'c',
-        caseModel: 'c',
-        additional: {
-            timestamps: false
-        },
-        directorys: config.directorys || './models',
-        lang: 'ts'
+  {
+    caseFile: 'c',
+    caseModel: 'c',
+    additional: {
+      timestamps: false
     },
-    pick(sequelizeConfig, ['host', 'dialect', 'port']))
+    directorys: config.directorys || './models',
+    lang: 'ts'
+  },
+  pick(sequelizeConfig, ['host', 'dialect', 'port']))
 );
 
 // process args control to run sequelize-auto sync
@@ -33,6 +33,6 @@ if (!isNil(options.sync)) auto.run();
 
 // export for program invoke
 module.export = () => {
-    auto.run();
-    return auto;
+  auto.run();
+  return auto;
 };
